5984b75873e50eaed9df23d54aac82de158273ed
hasna Oulad <ouladch@gmail.com>
2017-11-12 17:05:54 -0500

    adding the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,29 @@
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    description:  "Face au jardin, on découvre une salle lumineuse... et la cuisine d'Éric Frechon.",
+    stars:        3
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    description:  "Une valeur sûre que cette belle maison du 17e et les recettes de Jean-Christophe Rizet",
+    stars:        1
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    description:  "Oeil vif, geste sûr: impossible de distinguer, dans les créations de Frédéric Anton...",
+    stars:        3
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
